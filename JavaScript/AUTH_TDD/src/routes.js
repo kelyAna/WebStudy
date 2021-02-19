3908b1689e1243061b8099ed0e6bf816d8d55499
Ana Kely Lopes <kely@alu.ufc.br>
2021-02-19 00:21:55 -0300

    Add files

---
+++ JavaScript/AUTH_TDD/src/routes.js
@@ -0,0 +1,8 @@
+const routes = require('express')
+                        .Router();
+
+/*
+    Definição das rotas da nossa aplicação
+*/
+
+module.exports = routes;
---
+++ JavaScript/AUTH_TDD/src/server.js
@@ -0,0 +1,6 @@
+const app = require('./app')
+
+/*
+    Nossa aplicação estará na porta 3000
+*/
+app.listen(process.env.PORT || 3000);
