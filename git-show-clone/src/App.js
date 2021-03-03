be1001568fdfe4701220d40c1e8258f99c8a592a
Ana Kely Lopes <kely@alu.ufc.br>
2021-03-03 14:25:59 -0300

    Create project Git Show Clone

---
+++ git-show-clone/src/App.js
@@ -0,0 +1,13 @@
+import { Navigator } from "./common/Navigator/Navigator";
+
+import "./styles/global.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ git-show-clone/src/common/Navigator/Navigator.js
@@ -0,0 +1,20 @@
+import './Navigator.css'
+
+export function Navigator() {
+  return (
+    <>
+      <div className="navigator">
+        <div className="logo">
+          <img src="/suitcase.svg"/>
+        </div>
+        <div className="navigator-items">
+          <img src="https://github.com/kelyAna.png" alt="Ana Kely" />
+          <button>
+            <a>MY PROFILE</a>
+          </button>
+        </div>
+      </div>
+      <hr/>
+    </>
+  )
+}
